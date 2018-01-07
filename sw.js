1507ac44e0921ba6aa0f76606e8d185422d2e20c
Ardelean Calin <ardelean.calin@outlook.com>
2018-01-07 15:41:14 +0200

    Added index.js to cache & changed App Theme.

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 var CACHE_NAME = "my-site-cache-v1";
 var urlsToCache = [
     "/",
+    "/index.js",
     "/css/index.css",
     "/css/materialize.min.css",
     "/css/nouislider.min.css",
@@ -13,24 +14,38 @@ var urlsToCache = [
     "/js/jquery-3.2.1.min.js"
 ];
 
-self.addEventListener("install", function(event) {
-    // Perform install steps
-    event.waitUntil(
+self.addEventListener("install", function(e) {
+    console.log("[ServiceWorker] Install");
+    e.waitUntil(
         caches.open(CACHE_NAME).then(function(cache) {
-            console.log("Opened cache");
+            console.log("[ServiceWorker] Caching app shell");
             return cache.addAll(urlsToCache);
         })
     );
 });
 
-self.addEventListener("fetch", function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            // Cache hit - return response
-            if (response) {
-                return response;
-            }
-            return fetch(event.request);
+self.addEventListener("activate", function(e) {
+    console.log("[ServiceWorker] Activate");
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(
+                keyList.map(function(key) {
+                    if (key !== CACHE_NAME) {
+                        console.log("[ServiceWorker] Removing old cache", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener("fetch", function(e) {
+    console.log("[ServiceWorker] Fetch", e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
         })
     );
 });
