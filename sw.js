1788affca004fd4a6f01194df61b4b93fa2fa4f8
Ardelean Calin <ardelean.calin@outlook.com>
2018-01-07 21:52:12 +0200

    Offline support again...

--- sw.js
+++ sw.js
@@ -1,48 +1,53 @@
 var CACHE_NAME = "my-site-cache-v1";
 var urlsToCache = [
-    "/",
-    "/index.html",
-    "/index.js",
-    "/css/index.css",
-    "/css/materialize.min.css",
-    "/css/nouislider.min.css",
-    "/js/materialize.min.js",
-    "/js/jquery-3.2.1.min.js",
-    "/js/lodash.min.js",
-    "/js/wNumb.js",
-    "/js/nouislider.min.js",
-    "/js/BluetoothTerminal.js",
-    "/js/jquery-3.2.1.min.js"
+    ".",
+    "index.html",
+    "index.js",
+    "css/index.css",
+    "css/materialize.min.css",
+    "css/nouislider.min.css",
+    "js/materialize.min.js",
+    "js/jquery-3.2.1.min.js",
+    "js/lodash.min.js",
+    "js/wNumb.js",
+    "js/nouislider.min.js",
+    "js/BluetoothTerminal.js",
+    "js/jquery-3.2.1.min.js"
 ];
 
 self.addEventListener("install", function(e) {
     console.log("[ServiceWorker] Install");
+    self.addEventListener("install", function(event) {
+        event.waitUntil(
+            caches.open(CACHE_NAME).then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+        );
+    });
 });
 
 self.addEventListener("fetch", function(event) {
-    console.log("[ServiceWorker] - fetch", event.request.url);
     event.respondWith(
-        caches.open("mycache").then(function(cache) {
-            return cache.match(event.request).then(function(response) {
-                return (
-                    response ||
-                    fetch(event.request)
-                        .then(function(response) {
-                            console.log(
-                                "[ServiceWorker] - populating cache",
-                                event.request.url
-                            );
-                            cache.put(event.request, response.clone());
-                            return response;
-                        })
-                        .catch(function(error) {
-                            console.log(
-                                "[ServiceWorker] - There has been a problem with your fetch operation: ",
-                                error.message
-                            );
-                        })
-                );
-            });
+        caches.match(event.request).then(function(response) {
+            return response || fetchAndCache(event.request);
         })
     );
 });
+
+function fetchAndCache(url) {
+    return fetch(url)
+        .then(function(response) {
+            // Check if we received a valid response
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return caches.open(CACHE_NAME).then(function(cache) {
+                cache.put(url, response.clone());
+                return response;
+            });
+        })
+        .catch(function(error) {
+            console.log("Request failed:", error);
+            // You could return a custom offline 404 page here
+        });
+}
