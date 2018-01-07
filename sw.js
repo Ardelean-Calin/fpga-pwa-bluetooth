23117a087e252c979afccaa3e6cae1e6736a5291
Ardelean Calin <ardelean.calin@outlook.com>
2018-01-07 18:24:35 +0200

    testing

--- sw.js
+++ sw.js
@@ -15,21 +15,50 @@ var urlsToCache = [
     "/js/jquery-3.2.1.min.js"
 ];
 
-self.addEventListener("install", function(e) {
-    console.log("[ServiceWorker] Install");
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            console.log("[ServiceWorker] Caching app shell");
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
+// self.addEventListener("install", function(e) {
+//     console.log("[ServiceWorker] Install");
+//     e.waitUntil(
+//         caches.open(CACHE_NAME).then(function(cache) {
+//             console.log("[ServiceWorker] Caching app shell");
+//             return cache.addAll(urlsToCache);
+//         })
+//     );
+// });
 
-self.addEventListener("fetch", function(e) {
-    console.log("[ServiceWorker] Fetch", e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
-    );
+// self.addEventListener("activate", function(e) {
+//     console.log("[ServiceWorker] Activate");
+//     e.waitUntil(
+//         caches.keys().then(function(keyList) {
+//             return Promise.all(
+//                 keyList.map(function(key) {
+//                     if (key !== CACHE_NAME) {
+//                         console.log("[ServiceWorker] Removing old cache", key);
+//                         return caches.delete(key);
+//                     }
+//                 })
+//             );
+//         })
+//     );
+//     return self.clients.claim();
+// });
+
+self.addEventListener("fetch", function(event) {
+    console.log("[ServiceWorker] Fetch", event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+          .then(function(response) {
+            if(response){
+              console.log('Serve from cache', response);
+              return response;
+            }
+            return fetch(event.request)
+                .then(response =>
+                  caches.open(CURRENT_CACHES.prefetch)
+                    .then((cache) => {
+                      // cache response after making a request
+                      cache.put(event.request, response.clone());
+                      // return original response
+                      return response;
+                    })
+                )
 });
